0c2283178f07dce712db1004446fa2bd88b79b12
sarmadkhan1991 <43259363+sarmadkhan1991@users.noreply.github.com>
2019-12-18 15:31:58 -0700

    all steps completed!

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -7,24 +7,34 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
+import { connect } from 'react-redux';
+import { requestUserData } from '../../userReducer'
+import { requestBudgetData, addPurchase, removePurchase } from '../../budgetReducer';
 
 
 class Budget extends Component {
 
+  componentDidMount () {
+    this.props.requestUserData();
+    this.props.requestBudgetData();
+  }
+
   render() {
+    const { loading, purchases, budgetLimit } = this.props.budget;
+    const { firstName, lastName } = this.props.user;
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
-          <Nav />
+          <Nav firstName={firstName} lastName={lastName} />
           <div className='content-container'>
             <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
+              <AddPurchase addPurchase={this.props.addPurchase}/>
+              <DisplayPurchases purchases={this.props.budget.purchases} removePurchase={this.props.removePurchase}/>
             </div>
             <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
+              <Chart1 purchases={purchases} budgetLimit={budgetLimit}/>
+              <Chart2 purchases={purchases}/>
             </div>
           </div>
         </div>
@@ -33,4 +43,11 @@ class Budget extends Component {
   }
 }
 
-export default Budget;
+function mapStateToProps(reduxState) {
+  return {
+    budget: reduxState.budget,
+    user: reduxState.user
+  }
+}
+
+export default connect(mapStateToProps, {requestUserData, requestBudgetData, addPurchase, removePurchase})(Budget);
